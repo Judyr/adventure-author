503cd55563ba212fca6f7da0a6196d738813ac66
keiron.nicholson <keiron.nicholson@f0dab308-d534-0410-bd5f-8d6511b6bbc3>
2007-07-28 14:02:54 +0000

    License clarification.

--- trunk/Core/ConversationPage.cs
+++ trunk/Core/ConversationPage.cs
@@ -1,5 +1,4 @@
-﻿
-/*
+﻿/*
  *   This file is part of Adventure Author.
  *
  *   Adventure Author is copyright Heriot-Watt University 2006-2007.
@@ -16,6 +15,10 @@
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *   GNU General Public License for more details.
+ * 
+ *   Adventure Author is a plugin for Atari's Neverwinter Nights 2, a COMMERCIAL
+ *   product. Permission is given to link this GPL-covered plug-in with the 
+ *   non-free main program. 
  *
  *   You should have received a copy of the GNU General Public License
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
