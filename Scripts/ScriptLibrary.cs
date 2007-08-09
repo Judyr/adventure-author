0162f3278ba14067d531f4e75350a8378306f0bf
keiron.nicholson <keiron.nicholson@f0dab308-d534-0410-bd5f-8d6511b6bbc3>
2007-08-09 19:58:44 +0000

    Reworked ScriptLibrary to return conditions, and added all the main Conditions (I think)

--- Scripts/ScriptLibrary.cs
+++ Scripts/ScriptLibrary.cs
@@ -32,6 +32,8 @@ using AdventureAuthor.Core;
 using AdventureAuthor.Utils;
 using AdventureAuthor.Scripts;
 using NWN2Toolset.NWN2.Data;
+using NWN2Toolset.NWN2.Data.Instances;
+using NWN2Toolset.NWN2.Data.Templates;
 using NWN2Toolset.NWN2.Data.TypedCollections;
 using NWN2Toolset.NWN2.UI;
 using OEIShared.IO;
@@ -47,6 +49,25 @@ namespace AdventureAuthor.Scripts
 		private static string ownerName = "[OWNER]";
 			
 		
+		//TODO: do GetConditionalFunctor by calling and casting GetScriptFunctor and doing a bit more
+		//TODO: write a script to give player all feats and massive lore and automatically attach it upon the creation
+		//of a new adventure
+		
+		/// <summary>
+		/// Returns a conditional functor given a script name and an array of parameter arguments.
+		/// </summary>
+		/// <param name="scriptName">The name/resref of the script, e.g. gc_item_count (no file extension)</param>
+		/// <param name="args">The arguments to pass into the script method.</param>
+		/// <returns>Returns a conditional functor, or null if failed.</returns>
+		public static NWN2ConditionalFunctor GetConditionalFunctor(string scriptName, object[] args)
+		{
+			NWN2ConditionalFunctor conditionalFunctor = GetFunctor(scriptName, args);
+			
+			// TODO: Do stuff with AND, OR and NOT here.
+			
+			return conditionalFunctor;
+		}	
+		
 		/// <summary>
 		/// Returns a script functor given a script name and an array of parameter arguments.
 		/// </summary>
@@ -55,6 +76,18 @@ namespace AdventureAuthor.Scripts
 		/// <returns>Returns a script functor, or null if failed.</returns>
 		public static NWN2ScriptFunctor GetScriptFunctor(string scriptName, object[] args)
 		{
+			NWN2ScriptFunctor scriptFunctor = (NWN2ScriptFunctor)GetFunctor(scriptName, args);
+			return scriptFunctor;
+		}
+		
+		/// <summary>
+		/// Returns a script functor given a script name and an array of parameter arguments.
+		/// </summary>
+		/// <param name="scriptName">The name/resref of the script, e.g. ga_attack (no file extension)</param>
+		/// <param name="args">The arguments to pass into the script method.</param>
+		/// <returns>Returns a script functor, or null if failed.</returns>
+		private static NWN2ConditionalFunctor GetFunctor(string scriptName, object[] args)
+		{
 			try {
 				NWN2GameScript script = GetScript(scriptName);
 				if (script == null) {
@@ -62,30 +95,32 @@ namespace AdventureAuthor.Scripts
 					return null;
 				}		
 								
-				NWN2ScriptFunctor scriptFunctor = new NWN2ScriptFunctor();
-				scriptFunctor.Script = script.Resource;
+				NWN2ConditionalFunctor functor = new NWN2ConditionalFunctor();
+				functor.Script = script.Resource;
 				
-				foreach (object o in args) { // float, int, string or tag(?)
-					NWN2ScriptParameter param = new NWN2ScriptParameter();					
-					if (o is string) {
-						param.ParameterType = NWN2ScriptParameterType.String;
-						param.ValueString = (string)o;
-					}
-					else if (o is int) {
-						param.ParameterType = NWN2ScriptParameterType.Int;
-						param.ValueInt = (int)o;				
-					}
-					else if (o is float) {
-						param.ParameterType = NWN2ScriptParameterType.Float;
-						param.ValueFloat = (float)o;
-					}
-					else {
-						throw new ArgumentException("Was passed an argument of type " + o.GetType().ToString() + 
-						                            " which is not valid as a script parameter.");
-					}	
-					scriptFunctor.Parameters.Add(param);
-				}				
-				return scriptFunctor;
+				if (args != null) {
+					foreach (object o in args) { // float, int, string or tag(?)
+						NWN2ScriptParameter param = new NWN2ScriptParameter();					
+						if (o is string) {
+							param.ParameterType = NWN2ScriptParameterType.String;
+							param.ValueString = (string)o;
+						}
+						else if (o is int) {
+							param.ParameterType = NWN2ScriptParameterType.Int;
+							param.ValueInt = (int)o;				
+						}
+						else if (o is float) {
+							param.ParameterType = NWN2ScriptParameterType.Float;
+							param.ValueFloat = (float)o;
+						}
+						else {
+							throw new ArgumentException("Was passed an argument of type " + o.GetType().ToString() + 
+							                            " which is not valid as a script parameter.");
+						}	
+						functor.Parameters.Add(param);
+					}		
+				}
+				return functor;
 			}
 			catch (ArgumentException e) {
 				Say.Error(e);
@@ -117,6 +152,10 @@ namespace AdventureAuthor.Scripts
 		
 		public static string GetDescription(NWN2ScriptFunctor action)
 		{
+			if (action == null) {
+				return "Was given a blank action.";
+			}
+			
 			switch (action.Script.ResRef.Value) {
 				case "ga_attack":
 					string sAttacker = action.Parameters[0].ValueString;
@@ -136,14 +175,14 @@ namespace AdventureAuthor.Scripts
 					if (fDelay > 0.0f) {
 						s0.Append(fDelay.ToString() + " SECONDS LATER, ");
 					}
-					s0.Append("'" + sNewTag + "' APPEARS AT LOCATION '" + sLocationTag + "'.");
+					s0.Append("" + sNewTag + " APPEARS AT LOCATION " + sLocationTag + ".");
 					return s0.ToString();
 					
 				case "ga_death":
 					string deathtag = action.Parameters[0].ValueString;
 					int deathinstance = action.Parameters[1].ValueInt;
 					if (deathinstance > 0) {
-						return "EVERYTHING CALLED '" + deathtag + "' IS KILLED.";
+						return "EVERYTHING CALLED " + deathtag + " IS KILLED.";
 					}
 					else {
 						return deathtag.ToUpper() + " IS KILLED.";
@@ -173,35 +212,37 @@ namespace AdventureAuthor.Scripts
 					string destroyitemtag = action.Parameters[0].ValueString;
 					int destroyiteminstances = action.Parameters[1].ValueInt;
 					if (destroyiteminstances == -1) {
-						return "EVERY ITEM CALLED '" + destroyitemtag + "' IS REMOVED FROM INVENTORY.";
+						return "EVERY ITEM CALLED " + destroyitemtag + " IS REMOVED FROM INVENTORY.";
 					}
 					else if (destroyiteminstances == 1) {
 						return destroyitemtag + " IS REMOVED FROM INVENTORY.";
 					}
 					else {
-						return destroyiteminstances.ToString() + " ITEMS CALLED '" + destroyitemtag + "' ARE REMOVED FROM INVENTORY.";
+						return destroyiteminstances.ToString() + " ITEMS CALLED " + destroyitemtag + " ARE REMOVED FROM INVENTORY.";
 					}
 					
 				case "ga_destroy_party_henchmen":
 					return "ALL THE PLAYER'S HENCHMEN ARE REMOVED FROM THE AREA.";
-					
+										
 				case "ga_door_close":
 					string sTag = action.Parameters[0].ValueString;
-					return "DOOR '" + sTag + "' IS CLOSED.";
+					return "DOOR " + sTag + " IS CLOSED.";
 										
 				case "ga_door_open":
 					string sTag1 = action.Parameters[0].ValueString;
-					return "DOOR '" + sTag1 + "' IS OPENED.";
-					
-				case "ga_lock":
-					string locktag = action.Parameters[0].ValueString;
-					int lockstatus = action.Parameters[1].ValueInt;
-					if (lockstatus == 1) {
-						return "DOOR '" + locktag + "' IS LOCKED.";
+					return "DOOR " + sTag1 + " IS OPENED.";
+						
+				case "ga_force_exit":
+					string moves;
+					if (action.Parameters[2].ValueInt == 1) {
+						moves = "RUNS";
 					}
 					else {
-						return "DOOR '" + locktag + "' IS UNLOCKED.";
+						moves = "WALKS";
 					}
+					
+					return action.Parameters[0].ValueString + " " + moves + " TO " + action.Parameters[1].ValueString
+						+ " AND DISAPPEARS.";
 					
 				case "ga_give_gold":
 					int nGP = action.Parameters[0].ValueInt;
@@ -211,11 +252,20 @@ namespace AdventureAuthor.Scripts
 					string sTemplate1 = action.Parameters[0].ValueString;
 					int nQuantity = action.Parameters[1].ValueInt;
 					if (nQuantity != 1) {
-						return "PLAYER RECEIVES " + nQuantity + " COPIES OF '" + sTemplate1 + ".";
+						return "PLAYER RECEIVES " + nQuantity + " COPIES OF " + sTemplate1 + ".";
 					}
 					else {
 						return "PLAYER RECEIVES A " + sTemplate1 + ".";
 					}
+					
+				case "ga_global_float":
+					return "SET FLOAT VARIABLE " + action.Parameters[0].ValueString + " TO VALUE " + action.Parameters[1].ValueString;
+					
+				case "ga_global_int":
+					return "SET INT VARIABLE " + action.Parameters[0].ValueString + " TO VALUE " + action.Parameters[1].ValueString;
+						
+				case "ga_global_string":
+					return "SET STRING VARIABLE " + action.Parameters[0].ValueString + " TO VALUE " + action.Parameters[1].ValueString;
 					
 				case "ga_henchman_add": 
 					string sTarget1 = action.Parameters[0].ValueString;
@@ -231,17 +281,27 @@ namespace AdventureAuthor.Scripts
 					string thingthatjumps = action.Parameters[1].ValueString;
 					float timetowait = action.Parameters[2].ValueFloat;					
 					if (timetowait != 0.0) {
-						return timetowait.ToString() + " SECONDS LATER, '" + 
-							thingthatjumps.ToUpper() + "' JUMPS TO '" + jumpdestination.ToUpper() + "'.";
+						return timetowait.ToString() + " SECONDS LATER, " + 
+							thingthatjumps.ToUpper() + " JUMPS TO " + jumpdestination.ToUpper() + ".";
 					}
 					else {
-						return "'" + thingthatjumps.ToUpper() + "' JUMPS TO '" + jumpdestination.ToUpper() + "'.";
+						return "" + thingthatjumps.ToUpper() + " JUMPS TO " + jumpdestination.ToUpper() + ".";
 					}
 					
 				case "ga_jump_players":
 					string locationtojumpto = action.Parameters[0].ValueString;
-					return "PLAYER JUMPS TO LOCATION '" + locationtojumpto + "'.";
+					return "PLAYER JUMPS TO LOCATION " + locationtojumpto + ".";
 					
+				case "ga_lock":
+					string locktag = action.Parameters[0].ValueString;
+					int lockstatus = action.Parameters[1].ValueInt;
+					if (lockstatus == 1) {
+						return "DOOR " + locktag + " IS LOCKED.";
+					}
+					else {
+						return "DOOR " + locktag + " IS UNLOCKED.";
+					}
+									
 				case "ga_take_gold":
 					int goldtotake = action.Parameters[0].ValueInt;
 					return "PLAYER LOSES " + goldtotake + " PIECES OF GOLD.";
@@ -250,23 +310,107 @@ namespace AdventureAuthor.Scripts
 					string tagofitem = action.Parameters[0].ValueString;
 					int numberofitems = action.Parameters[0].ValueInt;
 					if (numberofitems == 1) {
-						return "PLAYER LOSES '" + tagofitem + "'.";
+						return "PLAYER LOSES " + tagofitem + ".";
 					}
 					else if (numberofitems == -1) {
-						return "PLAYER LOSES ALL ITEMS WITH TAG '" + tagofitem + "'.";
+						return "PLAYER LOSES ALL ITEMS WITH TAG " + tagofitem + ".";
 					}
 					else {
-						return "PLAYER LOSES " + numberofitems + " ITEMS WITH TAG '" + tagofitem + "'.";
+						return "PLAYER LOSES " + numberofitems + " ITEMS WITH TAG " + tagofitem + ".";
 					}
 					
 				default:
 					return "No description available (" + action.Script.ResRef.Value + ")";
 			}
 		}
-		
+			 	
 		public static string GetDescription(NWN2ConditionalFunctorCollection conditions)
 		{
-			return "IF A CERTAIN CONDITION IS FULFILLED";
+			if (conditions == null || conditions.Count == 0) {
+				return "Was given a blank set of conditions.";
+			}
+			if (conditions.Count > 1) {
+				Say.Warning("Only the first condition you have set will be described.");
+			}
+			
+			NWN2ConditionalFunctor condition = conditions[0];
+			
+			switch (condition.Script.ResRef.Value) {
+				case "gc_align_chaotic":
+					return "IF THE PLAYER'S ALIGNMENT IS CHAOTIC";
+					
+				case "gc_align_evil":
+					return "IF THE PLAYER IS EVIL";
+					
+				case "gc_align_good":
+					return "IF THE PLAYER IS GOOD";
+					
+				case "gc_align_lawful":
+					return "IF THE PLAYER'S ALIGNMENT IS LAWFUL";
+					
+				case "gc_check_gold":
+					return "IF THE PLAYER HAS AT LEAST " + condition.Parameters[0].ValueInt + " GOLD PIECES";
+					
+				case "gc_check_item":
+					return "IF THE PLAYER HAS THE ITEM " + condition.Parameters[0].ValueString;
+					
+				case "gc_dead":
+					return "IF " + condition.Parameters[0].ValueString + " IS DEAD";					
+										
+				case "gc_distance_pc":
+					return "IF THE DISTANCE BETWEEN THE PLAYER AND " + condition.Parameters[0].ValueString + 
+						" IN METRES IS " + condition.Parameters[1].ValueString;						
+					
+				case "gc_distance":
+					return "IF THE DISTANCE BETWEEN " + condition.Parameters[0].ValueString + " AND " + condition.Parameters[1].ValueString +
+						" IN METRES IS " + condition.Parameters[2].ValueString;	
+					
+				case "gc_global_float":
+					return "IF FLOAT VARIABLE " + condition.Parameters[0].ValueString + " IS OF VALUE " + condition.Parameters[1].ValueString;
+					
+				case "gc_global_int":
+					return "IF INT VARIABLE " + condition.Parameters[0].ValueString + " IS OF VALUE " + condition.Parameters[1].ValueString;
+						
+				case "gc_global_string":
+					return "IF STRING VARIABLE " + condition.Parameters[0].ValueString + " IS OF VALUE " + condition.Parameters[1].ValueString;
+					
+				case "gc_henchman":
+					if (condition.Parameters[1].ValueString != String.Empty) {
+						return "IF " + condition.Parameters[0].ValueString + " IS A HENCHMAN OF " + condition.Parameters[1].ValueString;
+					}
+					else {
+						return "IF " + condition.Parameters[0].ValueString + " IS THE PLAYER'S HENCHMAN";
+					}
+					
+				case "gc_is_enemy_near":
+					return "IF A HOSTILE CREATURE IS WITHIN " + condition.Parameters[0].ValueFloat + " METRES OF THE PLAYER";
+					
+				case "gc_is_female":
+					return "IF THE PLAYER IS FEMALE";
+					
+				case "gc_is_in_party":
+					return "IF " + condition.Parameters[0].ValueString + " IS THE PLAYER'S COMPANION";
+					
+				case "gc_is_male":
+					return "IF THE PLAYER IS MALE";
+					
+				case "gc_is_open":
+					return "IF " + condition.Parameters[0].ValueString + " IS OPEN";
+					
+				case "gc_item_count":
+					return "IF THE PLAYER HAS " + condition.Parameters[1].ValueString + 
+						   " COPIES OF ITEM " + condition.Parameters[0].ValueString + "";
+					
+				case "gc_journal_entry":
+					// TODO: Might be possible to do this better by checking the journal entry and getting its title, entries?
+					return "IF JOURNAL QUEST " + condition.Parameters[0].ValueString + " IS OF VALUE " + condition.Parameters[1].ValueString;
+					
+				case "gc_num_comps":
+					return "IF THE PLAYER HAS " + condition.Parameters[0].ValueString + " COMPANIONS";
+					
+				default:
+					return "No description available (" + condition.Script.ResRef.Value + ")";
+			}
 		}
 		
 		private static string GetOwnerIfBlank(string tag)
@@ -287,6 +431,18 @@ namespace AdventureAuthor.Scripts
 			else {
 				return tag.ToUpper();
 			}
+		}
+		
+		public static List<string> GetTags(NWN2InstanceCollection instances)
+		{
+			List<string> tags = new List<string>(instances.Count);
+			foreach (INWN2Instance instance in instances) {
+				string tag = ((INWN2Object)instance).Tag;
+				if (!tags.Contains(tag)) {
+					tags.Add(tag);
+				}
+			}
+			return tags;
 		}
 	}
 }
