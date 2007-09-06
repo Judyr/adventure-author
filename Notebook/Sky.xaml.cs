eb23d1dfc165e9259e356cc6d0843ee8be365600
keiron.nicholson <keiron.nicholson@f0dab308-d534-0410-bd5f-8d6511b6bbc3>
2007-09-06 16:36:20 +0000

    Added core classes for 'My Ideas'.

---
+++ Notebook/Sky.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AdventureAuthor.Notebook
+{
+    public partial class Sky : UserControl
+    {				
+		private string author;
+		public string Author {
+			get { return author; }
+		}
+		
+		public Sky()
+		{
+            InitializeComponent();
+            this.author = String.Empty;
+		}
+		    	
+		public Sky(List<Balloon> balloons) : this()
+        {           
+			Ellipse e = new Ellipse();
+			
+			
+			Random random = new Random();
+            foreach (Balloon b in balloons) {
+            	Canvas.SetLeft(b,random.Next(100,(int)SkyCanvas.Width-100));
+            	Canvas.SetTop(b,random.Next(100,(int)SkyCanvas.Height-100));
+            	this.SkyCanvas.Children.Add(b);
+            }
+        }
+    }
+}
