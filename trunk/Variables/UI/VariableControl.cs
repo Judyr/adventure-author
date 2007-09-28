65fb4918eac2471832902748e4ee96afcb548f57
keiron.nicholson <keiron.nicholson@f0dab308-d534-0410-bd5f-8d6511b6bbc3>
2007-09-28 00:54:01 +0000

    Added UI and logic for adding/editing/deleting variables - still needs fine-tuning.

---
+++ trunk/Variables/UI/VariableControl.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using NWN2Toolset.NWN2.Data;
+using AdventureAuthor.Core;
+
+namespace AdventureAuthor.Variables.UI
+{
+    public partial class VariableControl : System.Windows.Controls.UserControl
+    {
+    	private NWN2ScriptVariable var;    	
+		public NWN2ScriptVariable Var {
+			get { return var; }
+		}
+    	
+        public VariableControl(NWN2ScriptVariable variable)
+        {
+        	if (variable.VariableType != NWN2ScriptVariableType.String && variable.VariableType != NWN2ScriptVariableType.Int) {
+            	throw new ArgumentException("Currently you can only add string and integer variables - " 
+            	                            + var.VariableType.ToString() + " is invalid.");     
+        	}
+        	
+        	var = variable;
+        	
+            InitializeComponent();
+            
+            VariableNameTextBox.Text = var.Name;
+            VariableValueTextBox.Text = var.ValueString;
+        }
+        
+        private void OnClick_Delete(object sender, EventArgs ea)
+        {
+        	if (MessageBox.Show("Delete this variable?","Delete", MessageBoxButtons.OKCancel) == DialogResult.OK) {
+	            NWN2ScriptVarTable variables = Adventure.CurrentAdventure.Module.ModuleInfo.Variables;
+	            variables.Remove(var);
+	            ((System.Windows.Controls.ListBox)Parent).Items.Remove(this);
+        	}
+        }
+
+        private void OnClick_Edit(object sender, EventArgs ea)
+        {
+        	CreateEditVariableWindow window = new CreateEditVariableWindow(ref var);
+        	window.ShowDialog();
+        	
+        }
+    }
+}
--- trunk/Variables/UI/VariablesWindow.xaml.cs
+++ trunk/Variables/UI/VariablesWindow.xaml.cs
@@ -1,41 +1,42 @@
 ﻿using System;
 using System.Windows;
 using AdventureAuthor.Core;
+using AdventureAuthor.Utils;
 using NWN2Toolset.NWN2.Data;
 
 namespace AdventureAuthor.Variables.UI
 {
-    /// <summary>
-    /// Interaction logic for VariablesWindow.xaml
-    /// </summary>
-
     public partial class VariablesWindow : Window
     {
-
         public VariablesWindow()
         {
             InitializeComponent();
-            
-            NWN2ScriptVarTable variables = Adventure.CurrentAdventure.Module.ModuleInfo.Variables;
-            foreach (NWN2ScriptVariable var in variables) {
-            	if (var.VariableType == NWN2ScriptVariableType.String) {
-            		StringControl varControl = new StringControl(var);
-            		StringVariablesList.Items.Add(varControl);
-            	}
-            	else if (var.VariableType == NWN2ScriptVariableType.Int) {
-            		// todo
-            	}
-            	else {
-            		// todo
-            		// also this should be a switch statement rather than string of if/else
-            	}
-            }
+            Populate();
         }
         
         public void OnClick_AddVariable(object sender, EventArgs ea)
         {
-        	//todo
+        	NWN2ScriptVariable var = null;
+        	CreateEditVariableWindow window = new CreateEditVariableWindow(ref var);
+        	window.ShowDialog();
+        	if (var != null) {
+        		Say.Debug("Adding new variable: " + var.ToString());
+        		Adventure.CurrentAdventure.Module.ModuleInfo.Variables.Add(var);
+        		Populate();
+        		Say.Debug("New variable count: " + Adventure.CurrentAdventure.Module.ModuleInfo.Variables.Count.ToString());
+        	}
+        	else {
+        		Say.Debug("No new variable returned.");
+        	}
         }
-
+        
+        internal void Populate()
+        {
+            NWN2ScriptVarTable variables = Adventure.CurrentAdventure.Module.ModuleInfo.Variables;
+            foreach (NWN2ScriptVariable var in variables) {
+            	VariableControl varControl = new VariableControl(var);
+            	VariablesList.Items.Add(varControl);
+            }
+        }
     }
 }
