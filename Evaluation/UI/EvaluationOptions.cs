5f4055c9bdb5dff3ee6a6964ba203e26768184e7
keiron.nicholson <keiron.nicholson@f0dab308-d534-0410-bd5f-8d6511b6bbc3>
2008-01-16 19:32:30 +0000

    Improved UI, added ability to select and view evidence.

---
+++ Evaluation/UI/EvaluationOptions.cs
@@ -0,0 +1,44 @@
+/*
+ * Created by SharpDevelop.
+ * User: Kirn
+ * Date: 16/01/2008
+ * Time: 17:04
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+using System;
+
+namespace AdventureAuthor.Evaluation.UI
+{
+	/// <summary>
+	/// Description of EvaluationOptions.
+	/// </summary>
+	public static class EvaluationOptions
+	{
+		public enum ImageApps {
+			MicrosoftPaint,
+			Default
+		}
+		
+		private static ImageApps applicationToOpenImages = ImageApps.MicrosoftPaint;		
+		public static ImageApps ApplicationToOpenImages {
+			get { return applicationToOpenImages; }
+			set { 
+				applicationToOpenImages = value; 
+				OnChangedDefaultImageApplication(new EventArgs());
+			}
+		}
+		
+		public static event EventHandler ChangedDefaultImageApplication;
+		
+		private static void OnChangedDefaultImageApplication(EventArgs e) 
+		{
+			EventHandler handler = ChangedDefaultImageApplication;
+			if (handler != null) {
+				handler(null,e);
+			}
+		}
+		
+	}
+}
