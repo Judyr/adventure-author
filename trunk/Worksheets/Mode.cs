dc78923cf0c1f85659e101bed77fff4e51d20337
keiron.nicholson <keiron.nicholson@f0dab308-d534-0410-bd5f-8d6511b6bbc3>
2008-05-21 11:52:35 +0000

    Deleted Evaluation, now adding Worksheets again (hopefully).

---
+++ trunk/Worksheets/Mode.cs
@@ -0,0 +1,24 @@
+/*
+ * Created by SharpDevelop.
+ * User: Kirn
+ * Date: 11/02/2008
+ * Time: 19:48
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+using System;
+
+namespace AdventureAuthor.Evaluation
+{
+	/// <summary>
+	/// The mode to run the evaluation application in, allowing users to either 
+	/// build worksheets, fill them in or discuss completed worksheets.
+	/// </summary>
+	public enum Mode
+	{		
+    	Design, // design and build a worksheet from scratch, or edit an existing worksheet
+    	Complete, // fill in the answer fields in an existing worksheet
+    	Discuss // add comments on a worksheet which has been filled in
+	}
+}
