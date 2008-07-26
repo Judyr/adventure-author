546d7071aa949b6117b0c78d03d61622bff2c3b7
keiron.nicholson <keiron.nicholson@f0dab308-d534-0410-bd5f-8d6511b6bbc3>
2008-07-26 18:25:57 +0000

    Added My Tasks.

---
+++ trunk/My Tasks/Task.cs
@@ -0,0 +1,23 @@
+/*
+ * Created by SharpDevelop.
+ * User: Kirn
+ * Date: 24/07/2008
+ * Time: 17:15
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+using System;
+
+namespace AdventureAuthor.Tasks
+{
+	/// <summary>
+	/// Description of Task.
+	/// </summary>
+	public class Task
+	{
+		public Task()
+		{
+		}
+	}
+}
---
+++ trunk/My Tasks/TaskPad.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AdventureAuthor.Tasks
+{
+    /// <summary>
+    /// Interaction logic for TaskPad.xaml
+    /// </summary>
+
+    public partial class TaskPad : UserControl
+    {
+		private Color[] allColors;
+		private Random random;
+		
+		
+		public TaskPad()
+		{
+			Type colorsType = typeof(Colors);
+			PropertyInfo[] properties = colorsType.GetProperties();
+			allColors = new Color[properties.Length];
+			for (int i = 0; i < properties.Length; i++) {
+				PropertyInfo property = properties[i];
+				Color color = (Color)property.GetValue(colorsType,null);
+				allColors[i] = color;
+			}
+			random = new Random();
+				
+			InitializeComponent();
+			
+			ScaleTransform largerDogEar = new ScaleTransform(1.3,1.3,40,40);
+						
+			nextPageButton.MouseEnter += delegate 
+			{
+				nextPageButton.RenderTransform = largerDogEar;
+			};
+						
+			nextPageButton.MouseLeave += delegate 
+			{
+				nextPageButton.RenderTransform = null;
+			};
+			
+			nextPageButton.MouseDown += delegate 
+			{  
+				
+			};
+		}
+		
+		
+		private void SetBackgroundColourRandomly()
+		{
+			int randomNumber = random.Next(0,allColors.Length-1);
+			Background = new SolidColorBrush(allColors[randomNumber]);
+		}
+    }
+}
