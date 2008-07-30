542ec764731c52aa87322656f2b080c827027414
keiron.nicholson <keiron.nicholson@f0dab308-d534-0410-bd5f-8d6511b6bbc3>
2008-07-30 09:04:40 +0000

    TaskPad displays a TaskCollection through data binding, but it seems slow.

--- trunk/My Tasks/TaskCollection.cs
+++ trunk/My Tasks/TaskCollection.cs
@@ -8,19 +8,23 @@
  */
 
 using System;
+using System.Xml.Serialization;
 using System.Collections.Generic;
+using AdventureAuthor.Utils;
 
 namespace AdventureAuthor.Tasks
 {
 	/// <summary>
 	/// A collection of tasks for a task manager application. 
 	/// </summary>
+	[Serializable]
 	public class TaskCollection
-	{
+	{	
 		/// <summary>
 		/// The user who created this task collection.
 		/// </summary>
 		private string creator;
+		[XmlAttribute("Creator")]
 		public string Creator {
 			get { return creator; }
 			set { creator = value; }
@@ -31,8 +35,10 @@ namespace AdventureAuthor.Tasks
 		/// The time this task collection was created.
 		/// </summary>
 		private DateTime created;
+		[XmlAttribute("Created")]
 		public DateTime Created {
 			get { return created; }
+			set { created = value; }
 		}
 		
 		
@@ -40,13 +46,37 @@ namespace AdventureAuthor.Tasks
 		/// A list of every task in this collection. 
 		/// </summary>
 		private List<Task> tasks;
+		[XmlArray]
 		public List<Task> Tasks {
 			get { return tasks; }
+			set { tasks = value; } //to enable TwoWay data binding
 		}
 		
 		
-		public TaskCollection()
+		/// <summary>
+		/// Create a new task collection.
+		/// </summary>
+		public TaskCollection() : this(new List<Task>())
 		{
+		}
+		
+		
+		/// <summary>
+		/// Create a new task collection.
+		/// </summary>
+		public TaskCollection(List<Task> tasks) : this(tasks,User.GetCurrentUserName(),DateTime.Now)
+		{
+		}
+		
+		
+		/// <summary>
+		/// Create a new task collection.
+		/// </summary>
+		public TaskCollection(List<Task> tasks, string creator, DateTime created)
+		{
+			this.tasks = tasks;
+			this.creator = creator;
+			this.created = created;
 		}
 	}
 }
